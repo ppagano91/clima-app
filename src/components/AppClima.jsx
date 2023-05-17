6cbc0887f02d631cb2262554d028c8bf19dbd188
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-17 09:42:37 -0300

    Ajustes y estructura de proyecto

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Formulario from "./Formulario";
+
+const AppClima = () => {
+  return (
+    <>
+      <main className="dos-columnas">
+        <Formulario />
+      </main>
+    </>
+  );
+};
+
+export default AppClima;
