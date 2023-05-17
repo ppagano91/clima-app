c68604376c8022bf9d45c6a72bcf6094706ff131
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-17 09:56:41 -0300

    Creando Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { PAISES } from "../constants";
 
 const Formulario = () => {
-  return <div>Formulario</div>;
+  return (
+    <div className="contenedor">
+      <form>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input type="text" id="ciudad" name="ciudad" />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">País</label>
+          <select name="pais" id="pais">
+            <option value="">-- Seleccione un país --</option>
+            {PAISES.map((pais) => (
+              <option key={pais.codigo} value={pais.codigo}>
+                {pais.nombre}
+              </option>
+            ))}
+          </select>
+        </div>
+        <input type="submit" value="Consultar Clima" />
+      </form>
+    </div>
+  );
 };
 
 export default Formulario;
